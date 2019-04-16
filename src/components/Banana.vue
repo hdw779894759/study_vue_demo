81761ad50918e6411c001f125c084af22815ec18
hdw779894759 <779894759@qq.com>
2019-04-16 15:50:10 +0800

    init

---
+++ src/components/Banana.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <button @click="addOne">add one</button>
+    <button @click="minusOne">minus one</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        msg: 'i am banana',
+        price: 15
+      }
+    },
+    methods:{
+      addOne() {
+        this.$store.commit('increment', this.price);
+      },
+      minusOne() {
+        this.$store.commit('decrement', this.price);
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  h1, h2 {
+    font-weight: normal;
+  }
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+  a {
+    color: #42b983;
+  }
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>{{$route.params.color}}</p>
+    <button @click="getParam">get param</button>
+
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  methods:{
+    getParam() {
+      // 打印路由中获取的参数
+      console.log(this.$route.params);
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
