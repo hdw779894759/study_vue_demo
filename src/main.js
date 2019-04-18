a397bfd7457f2ec4d246f599a940134ca2bea3d7
hdw779894759 <779894759@qq.com>
2019-04-18 09:27:22 +0800

    首页slide栏布局和数据渲染完成

--- src/main.js
+++ src/main.js
@@ -1,13 +1,30 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+// 引入组件
 import Vue from 'vue'
 import Layout from './components/layout'
+import VueRouter from 'vue-router'
+import IndexPage from './pages/index'
 
-Vue.config.productionTip = false
+// 注册组件
+Vue.use(VueRouter)
+
+// 实例化组件
+let router = new VueRouter({
+  mode: 'history',
+  routes:[
+    {
+      // 根目录使用index页面,【视图从父节点的layout中使用】
+      path: '/',
+      component:IndexPage
+    }
+  ]
+});
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  router,
   components: { Layout },
   // 将layout组件渲染到<Layout/>模板中
   template: '<Layout/>'
